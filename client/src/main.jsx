8838d4e4231f21e66e58294ae8e7fb2f4c544458
TheOneWithChair <cherry.studies19@gmail.com>
2025-03-16 01:35:47 +0530

    Add @vercel/analytics dependency and integrate it into the main application

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.jsx'
 import AppContextProvider from './context/AppContext.jsx'
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')).render(
    <BrowserRouter>
    <AppContextProvider>
     <App />
+    <Analytics />
     </AppContextProvider>
    </BrowserRouter>
   
